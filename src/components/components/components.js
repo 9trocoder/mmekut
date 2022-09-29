aee9a5c8c5da1b00b47f8860c4951dcf859ba48e
Emmanuel Oladimeji Adeyemi <46575938+9trocoder@users.noreply.github.com>
2022-09-29 19:59:05 +0300

    update

--- src/components/components/components.js
+++ src/components/components/components.js
@@ -1,4 +1,5 @@
 import React from "react";
+import PropTypes from "prop-types";
 import "./components.css";
 
 export const HeaderBar = ({
@@ -353,4 +354,37 @@ export const ChatBottomNav = ({
       </div>
     </>
   );
+};
+
+export const ProgressBar = (props) => {
+  const { progress, circleTwoStroke } = props;
+  let dsd = `calc(${progress} * 31.4 / 100) 31.4`;
+
+  return (
+    <>
+      <div className="progress__container">
+        <svg height="10" width="10" viewBox="0 0 20 20">
+          <circle r="10" cx="10" cy="10" fill="white" />
+          <circle
+            r="5"
+            cx="10"
+            cy="10"
+            fill="transparent"
+            stroke="#0184fb"
+            stroke-width="10"
+            stroke-dasharray={dsd}
+            transform="rotate(-90) translate(-20)"
+          />
+        </svg>
+      </div>
+    </>
+  );
+};
+
+ProgressBar.propTypes = {
+  size: PropTypes.number.isRequired,
+  progress: PropTypes.number.isRequired,
+  strokeWidth: PropTypes.number.isRequired,
+  circleOneStroke: PropTypes.string.isRequired,
+  circleTwoStroke: PropTypes.string.isRequired,
 };
