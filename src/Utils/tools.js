c255dda6e7482d03a3816f2d0fdbd5d07343933b
Emmanuel Oladimeji Adeyemi <46575938+9trocoder@users.noreply.github.com>
2022-09-30 16:40:35 +0300

    update

--- src/Utils/tools.js
+++ src/Utils/tools.js
@@ -169,8 +169,8 @@ export const deleteIcon = (
 
 export const deliveredIcon = (
   <svg
-    width="18"
-    height="18"
+    width="14"
+    height="14"
     viewBox="0 0 18 18"
     fill="none"
     xmlns="http://www.w3.org/2000/svg"
