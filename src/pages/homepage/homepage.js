e4baad6d8127b9ac6f396eb4b4145d16096e7bbe
Emmanuel Oladimeji Adeyemi <46575938+9trocoder@users.noreply.github.com>
2022-09-30 16:33:03 +0300

    Update homepage.js

--- src/pages/homepage/homepage.js
+++ src/pages/homepage/homepage.js
@@ -89,6 +89,10 @@ function Homepage() {
 
       <div className="homepagethechat__cnt">
         <ChatCardHomepage userImage={p1image} userStatus={true} userName="Alexander" messageStatus={deliveredIcon} messageTime="11:44 AM" message="Ok, I'll call you when i get there. and bring more snacks" />
+        <ChatCardHomepage userImage={p2image} userStatus={true} userName="Joseph Ayodele" messageStatus={deliveredIcon} messageTime="11:44 AM" message="Start Coding na" />
+        <ChatCardHomepage userImage={p3image} userStatus={true} userName="Mosope" messageStatus={deliveredIcon} messageTime="11:44 AM" message="On the home screen, that first content, when you click on the 'view task', it will take you here." />
+        <ChatCardHomepage userImage={p4image} userStatus={true} userName="Oladimeji Emmanuel" messageStatus={deliveredIcon} messageTime="11:44 AM" message="The containers are added to enable the user click on the group" />
+        <ChatCardHomepage userImage={p5image} userStatus={true} userName="Mmmekut Mfon" messageStatus={deliveredIcon} messageTime="11:44 AM" message="See here ....the task screen emmanuel adeyemi" />
       </div>
     </>
   );
