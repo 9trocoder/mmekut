f21b32815dd3c199b6d492b9f5f29ba90b79e81b
Emmanuel Oladimeji Adeyemi <46575938+9trocoder@users.noreply.github.com>
2022-10-07 16:58:03 +0300

    update

--- src/components/GroupsCard.js
+++ src/components/GroupsCard.js
@@ -13,51 +13,51 @@ function GroupsCard({
         <div className="groupsCard__cnt-top">
           <div className="groupsCard__cnt-topleft">
             <p className="groupsCard__cnt-topgroupname">{groupName}</p>
-           
           </div>
           <div className="groupsCard__cnt-topnotification">
             {notificationIcon}
           </div>
         </div>
         <div className="letseeamlike">
-             {groupUserImages.length > 4 ? (
-          <div className="selected__image_cntt">
-            {groupUserImages.slice(0, 4).map((item, index) => (
-              <>
-                <img
-                style={{width: "40px", height: "40px"}}
-                  className={`image${index}`}
-                  key={index}
-                  src={item}
-                  alt=""
-                />
-                
-              </>
-            ))}
-            <div style={{width: "40px", height: "40px"}} className="selected__image-remaining">
-              {groupUserImages.length - 4}
+          {groupUserImages.length > 4 ? (
+            <div className="selected__image_cntt">
+              {groupUserImages.slice(0, 4).map((item, index) => (
+                <>
+                  <img
+                    style={{ width: "40px", height: "40px" }}
+                    className={`image${index}`}
+                    key={index}
+                    src={item}
+                    alt=""
+                  />
+                </>
+              ))}
+              <div
+                style={{ width: "40px", height: "40px" }}
+                className="selected__image-remaining"
+              >
+                {groupUserImages.length - 4}
+              </div>
             </div>
-          </div>
-        ) : (
-          <div className="selected__image_cntt">
-            {groupUserImages.map((item, index) => (
-              <>
-                <img
-                style={{width: "40px", height: "40px"}}
-                  className={`image${index}`}
-                  key={index}
-                  src={item}
-                  alt=""
-                />
-              </>
-            ))}
-          </div>
-        )}
-         <p className="groupsCard__cnt-topgroupmembers">
-              {groupUserImages.length} Members
-            </p>
+          ) : (
+            <div className="selected__image_cntt">
+              {groupUserImages.map((item, index) => (
+                <>
+                  <img
+                    style={{ width: "40px", height: "40px" }}
+                    className={`image${index}`}
+                    key={index}
+                    src={item}
+                    alt=""
+                  />
+                </>
+              ))}
+            </div>
+          )}
+          <p className="groupsCard__cnt-topgroupmembers">
+            {groupUserImages.length} Members
+          </p>
         </div>
-       
       </div>
     </>
   );
