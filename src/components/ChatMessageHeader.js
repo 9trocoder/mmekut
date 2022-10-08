25c885642eee14c91b80015704b3c1cbc410e838
Emmanuel Oladimeji Adeyemi <46575938+9trocoder@users.noreply.github.com>
2022-10-08 11:39:08 +0300

    updated mmekut logo

---
+++ src/components/ChatMessageHeader.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./components.css";
+
+function ChatMessageModal({children, handleCloseClick, closeIcon, modalName, userName, userStatus, searchIcon, moreIcon, zIndex}) {
+  return <>
+        <div className="chatmessagemodal__cnt">
+            <div className="chatmessagemodal__cnt-body">
+                <div className="chatmessagemodal">
+                    <div className="chatmessagemodal__cnt-top">
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
+  
+  </>;
+}
+
+export default ChatMessageModal;
