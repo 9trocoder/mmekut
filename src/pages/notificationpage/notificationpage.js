02cd243b43e138f5b20c4009dfbad2f745fb26cd
Emmanuel Oladimeji Adeyemi <46575938+9trocoder@users.noreply.github.com>
2022-10-25 20:26:09 +0100

    update on the notification page

--- src/pages/notificationpage/notificationpage.js
+++ src/pages/notificationpage/notificationpage.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import HomepageHeader from "../../components/HompageHeader";
 import p2image from "../../assets/images/p2.png";
@@ -15,6 +15,8 @@ import {
 import BottomNav from "../../components/BottomNav";
 
 function Notificationpage() {
+  const [activeNotificationButton, setActiveNotificationButton] =
+    useState("chat");
   let navigate = useNavigate();
   const handleProfileClick = () => {
     navigate("/task");
@@ -32,6 +34,31 @@ function Notificationpage() {
   return (
     <>
       <HomepageHeader profileImage={p2image} calendarIcon={otherIcon} />
+
+      <div className="workspacepage__cnt">
+        <div className="chatpage__cnt-top">
+          <div
+            onClick={() => setActiveNotificationButton("chat")}
+            className={
+              activeNotificationButton === "chat"
+                ? "chatpage__cnt-allbutton"
+                : "chatpage__cnt-allbuttonnotactive"
+            }
+          >
+            <button>Chat</button>
+          </div>
+          <div
+            onClick={() => setActiveNotificationButton("task")}
+            className={
+              activeNotificationButton === "task"
+                ? "chatpage__cnt-allbutton"
+                : "chatpage__cnt-allbuttonnotactive"
+            }
+          >
+            <button>Tasks</button>
+          </div>
+        </div>
+      </div>
       <BottomNav
         showAddButton={false}
         homeIcon={homeIconNotActive}
