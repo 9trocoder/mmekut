39798d2689f14444b66cdd281201479fd859c9ec
Emmanuel Oladimeji Adeyemi <46575938+9trocoder@users.noreply.github.com>
2022-10-31 21:14:04 +0300

    added project card

---
+++ src/components/ProjectsCard.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function ProjectsCard({
+  projectName,
+  projectCategory,
+  projectpercentage,
+  projectColor,
+  handleClick,
+}) {
+  return <div>ProjectsCard</div>;
+}
+
+export default ProjectsCard;
--- src/pages/projectpage/projectpage.js
+++ src/pages/projectpage/projectpage.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import ChatMessageModal from "../../components/ChatMessageHeader";
+import p3 from "../../assets/images/p3.png";
 import {
   addIconWhiteSmall,
   chatActive,
@@ -18,6 +19,25 @@ function Projectpage() {
         userName="Projects"
         // searchIcon={chatActive}
       >
+        <div className="projectpage__cnt">
+          <div className="projectpage__items">
+            <div className="projectpage__items-top">
+              <img src={p3} alt="" />
+              <div className="projectpage__items-topmiddle">
+                <p className="projectpage__name">Ushy Dashboard</p>
+                <p className="projectpage__category">Design</p>
+              </div>
+              <div className="projectpage__workcount">
+                <p>15/20</p>
+              </div>
+            </div>
+            <div className="projectpage__items-bottom">
+              <div className="projectpage__items-progress">
+                <div className="projectpage__items-percentage" />
+              </div>
+            </div>
+          </div>
+        </div>
         <button onClick={() => {}} className="showaddWorkspace">
           {addIconWhiteSmall}
         </button>
