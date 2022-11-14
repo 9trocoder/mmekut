56f72d19c8536855b8b9392d4feed749d9b19664
Emmanuel Oladimeji Adeyemi <46575938+9trocoder@users.noreply.github.com>
2022-11-14 23:19:02 +0300

    Update calendarpage.js

--- src/pages/calendarpage/calendarpage.js
+++ src/pages/calendarpage/calendarpage.js
@@ -15,7 +15,12 @@ import {
   startOfToday,
 } from "date-fns";
 import ChatMessageModal from "../../components/ChatMessageHeader";
-import { clockIcon, headerBackIcon, otherIcon, otherIconTask } from "../../Utils/tools";
+import {
+  clockIcon,
+  headerBackIcon,
+  otherIcon,
+  otherIconTask,
+} from "../../Utils/tools";
 import p1image from "../../assets/images/p1.png";
 import p2image from "../../assets/images/p2.png";
 import p3image from "../../assets/images/p3.png";
@@ -109,9 +114,39 @@ function Calendarpage() {
       >
         <div className="calendarpage__cnt">
           <div className="calendarpage__cnt-top">
-            <button onClick={previousMonth}>previous</button>
+            <button onClick={previousMonth}>
+              <svg
+                width="15"
+                height="15"
+                viewBox="0 0 15 15"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M8.84182 3.13514C9.04327 3.32401 9.05348 3.64042 8.86462 3.84188L5.43521 7.49991L8.86462 11.1579C9.05348 11.3594 9.04327 11.6758 8.84182 11.8647C8.64036 12.0535 8.32394 12.0433 8.13508 11.8419L4.38508 7.84188C4.20477 7.64955 4.20477 7.35027 4.38508 7.15794L8.13508 3.15794C8.32394 2.95648 8.64036 2.94628 8.84182 3.13514Z"
+                  fill="currentColor"
+                  fill-rule="evenodd"
+                  clip-rule="evenodd"
+                ></path>
+              </svg>
+            </button>
             <h2>{format(firstDayCurrentMonth, "MMMM yyyy")}</h2>
-            <button onClick={nextMonth}>Next</button>
+            <button onClick={nextMonth}>
+              <svg
+                width="15"
+                height="15"
+                viewBox="0 0 15 15"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M6.1584 3.13508C6.35985 2.94621 6.67627 2.95642 6.86514 3.15788L10.6151 7.15788C10.7954 7.3502 10.7954 7.64949 10.6151 7.84182L6.86514 11.8418C6.67627 12.0433 6.35985 12.0535 6.1584 11.8646C5.95694 11.6757 5.94673 11.3593 6.1356 11.1579L9.565 7.49985L6.1356 3.84182C5.94673 3.64036 5.95694 3.32394 6.1584 3.13508Z"
+                  fill="currentColor"
+                  fill-rule="evenodd"
+                  clip-rule="evenodd"
+                ></path>
+              </svg>
+            </button>
           </div>
           <div className="calendarpage__cnt-daylist">
             <p className="calendardaylist">SUN</p>
@@ -139,13 +174,18 @@ function Calendarpage() {
             ))}
           </div>
           <section className="calendarpage__cnt-daydatetitle">
-            <h2>Taskes Scheduled for {' '} <time datetime={format(selectedDay, 'yyyy-MM-dd')}>{format(selectedDay, 'MMM dd, yyy')}</time></h2>
+            <h2>
+              Taskes Scheduled for{" "}
+              <time datetime={format(selectedDay, "yyyy-MM-dd")}>
+                {format(selectedDay, "MMM dd, yyy")}
+              </time>
+            </h2>
 
             <div className="taskesdlist">
               {selectedDayTasks.length > 0 ? (
                 selectedDayTasks.map((task) => {
-                  let startDateTime = parseISO(task.startDatetime)
-                  let endDateTime = parseISO(task.endDatetime)
+                  let startDateTime = parseISO(task.startDatetime);
+                  let endDateTime = parseISO(task.endDatetime);
                   return (
                     <div>
                       <div className="">
@@ -153,21 +193,27 @@ function Calendarpage() {
                           <p>{task.task}</p>
                           <p>{task.projectName}</p>
                         </div>
-                        <div className="">
-                          {otherIconTask}
-                        </div>
+                        <div className="">{otherIconTask}</div>
                       </div>
                       <div className="">
                         <div className="">
                           {clockIcon}
-                          <p><time datetime={task.startDatetime}>{format(startDateTime, 'h:mm a ')}</time>{' '}-{' '} <time datetime={task.endDatetime}>{format(endDateTime, 'h:mm a')}</time></p>
+                          <p>
+                            <time datetime={task.startDatetime}>
+                              {format(startDateTime, "h:mm a ")}
+                            </time>{" "}
+                            -{" "}
+                            <time datetime={task.endDatetime}>
+                              {format(endDateTime, "h:mm a")}
+                            </time>
+                          </p>
                         </div>
                         <div className="">
                           <img src={task.imageUrl} alt="" />
                         </div>
                       </div>
                     </div>
-                  )
+                  );
                 })
               ) : (
                 <p>No Task for today.</p>
