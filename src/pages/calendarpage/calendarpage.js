d47a93404a52c2afc0e7d000ede7170507bd332e
Emmanuel Oladimeji Adeyemi <46575938+9trocoder@users.noreply.github.com>
2022-11-14 15:49:03 +0300

    Update calendarpage.js

--- src/pages/calendarpage/calendarpage.js
+++ src/pages/calendarpage/calendarpage.js
@@ -1,23 +1,38 @@
-import React from 'react'
-import { useNavigate } from 'react-router-dom'
-import ChatMessageModal from '../../components/ChatMessageHeader'
-import { headerBackIcon, otherIcon } from '../../Utils/tools'
-import "./calendarpage.css"
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import {
+  add,
+  eachDayOfInterval,
+  endOfMonth,
+  format,
+  getDay,
+  isEqual,
+  isSameDay,
+  isSameMonth,
+  isToday,
+  parseISO,
+  startOfToday,
+} from "date-fns";
+import ChatMessageModal from "../../components/ChatMessageHeader";
+import { headerBackIcon, otherIcon } from "../../Utils/tools";
+import "./calendarpage.css";
 
 function Calendarpage() {
-  let navigate = useNavigate()
+  let navigate = useNavigate();
   const handleCloseClick = () => {
     navigate(-1);
   };
   return (
     <>
-      <ChatMessageModal zIndex={10008} closeIcon={headerBackIcon} handleCloseClick={handleCloseClick} userName="Calendar" moreIcon={otherIcon}>
-
-
-
-      </ChatMessageModal>
+      <ChatMessageModal
+        zIndex={10008}
+        closeIcon={headerBackIcon}
+        handleCloseClick={handleCloseClick}
+        userName="Calendar"
+        moreIcon={otherIcon}
+      ></ChatMessageModal>
     </>
-  )
+  );
 }
 
-export default Calendarpage
+export default Calendarpage;
